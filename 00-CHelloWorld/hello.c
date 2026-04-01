de572272b5f2dd9fb05b79c40afa822f4f33dfb4
Fabricio Calvano Viola <fcalvanoviola@frba.utn.edu.ar>
2026-04-01 16:46:15 -0300

    Agregando archivos del TP00

---
+++ 00-CHelloWorld/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    printf("Hello, World!\n");
+    printf("Version del lenguaje: %ld\n", __STDC_VERSION__);
+    return EXIT_SUCCESS;
+}
